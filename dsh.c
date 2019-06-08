e980a623a272137242bf90f29028227d5710f6f5
dmb <d.babiak@gmail.com>
2019-06-07 23:03:58 -0700

    cd

--- dsh.c
+++ dsh.c
@@ -4,22 +4,12 @@
 #include <string.h>
 #include <unistd.h>
 
-bool streq(const char *s1, const char *s2) {
-  return (!s1 && !s2) || (s1 && s2 && (0 == strcmp(s1, s2)));
-}
-
-void _pwd() {
-  char buf[256];
-  printf("%s\n", getcwd(buf, sizeof buf));
-}
-
-void exec_cmd(char *cmd) {
-  if (streq(cmd, "pwd"))
-    _pwd();
-}
-
 bool is_whitespace(char c) {
   return c == ' ' || c == '\n' || c == '\t' || c == '\r';
+}
+
+bool streq(const char *s1, const char *s2) {
+  return (!s1 && !s2) || (s1 && s2 && (0 == strcmp(s1, s2)));
 }
 
 char *rstrip(char *s) {
@@ -27,6 +17,43 @@ char *rstrip(char *s) {
   while (n >= 0 && is_whitespace(s[n]))
     s[n--] = '\0';
   return s;
+}
+
+bool startswith(char *target, char *prefix) {
+  size_t N = strlen(prefix);
+  if (strlen(target) < N)
+    return false;
+
+  for (int i = 0; i < N; i++)
+    if (target[i] != prefix[i])
+      return false;
+
+  return true;
+}
+
+void _pwd() {
+  char buf[256];
+  printf("%s\n", getcwd(buf, sizeof buf));
+}
+
+void _cd(const char *cmd) {
+  char path[256];
+  memset(path, 0, sizeof path);
+  int n = sscanf(cmd, "cd %s", path);
+  assert(n == 1);
+  int err = chdir(path);
+  assert(!err);
+  printf("err: %d\n", err);
+}
+
+void exec_cmd(char *cmd) {
+  rstrip(cmd);
+  if (streq(cmd, "pwd")) 
+    _pwd();
+  else if (startswith(cmd, "cd "))
+    _cd(cmd);
+  else
+    printf("wtf: %s\n", cmd);
 }
 
 int main(int argc, char *argv[]) {
@@ -42,9 +69,7 @@ int main(int argc, char *argv[]) {
     if (NULL == fgets(line, sizeof line, stdin)) 
       break;
 
-    exec_cmd(rstrip(line));
-    //fputs(line, stdout);
-    //fputs(line, stderr);
+    exec_cmd(line);
   }
   printf("\n");
 }
